adbaaa4d930e54fd1a428474f081f30bb0eea3f3
yonlu <lss.emailbox@gmail.com>
2022-01-03 18:43:18 -0500

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8'
+}
